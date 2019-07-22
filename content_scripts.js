1cf9b760cfb9cdeebecbd0bf6ae8a3061ae0ba8c
yiyuezhuo <616271835@163.com>
2019-07-22 20:32:00 +0800

    init

---
+++ content_scripts.js
@@ -0,0 +1,11 @@
+/*
+var element = document.querySelector('div.Card');
+//element.parentElement.removeChild(element);
+element.style.display='none'
+
+var element = document.querySelector('div.GlobalSideBar');
+element.style.display='none'
+
+var element = document.querySelector('header.AppHeader');
+element.style.display='none'
+*/
